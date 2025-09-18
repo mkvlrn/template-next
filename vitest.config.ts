93150ab6e7db0c0e24ea3751d52fcba9c66ad59a
Mike Valeriano <mkvlrn@gmail.com>
2025-09-18 19:18:27 -0300

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,26 +1,34 @@
+import process from "node:process";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  base: "https://mkvlrn.github.io/template-next/",
+function getConfig() {
+  const withIntegration = process.env["INTEGRATION"] === "true";
+  const testFiles = withIntegration
+    ? ["./src/**/*.{test,spec}.{ts,tsx}"]
+    : ["./src/**/*.test.{ts,tsx}"];
 
-  plugins: [react()],
+  return defineConfig({
+    plugins: [react()],
 
-  test: {
-    include: ["src/**/*.test.{ts,tsx}"],
-    reporters: ["verbose"],
-    watch: false,
-    coverage: {
-      all: true,
-      clean: true,
-      cleanOnRerun: true,
-      include: ["src"],
-      exclude: ["src/**/*.test.{ts,tsx}", "src/main.{ts,tsx}"],
+    test: {
+      include: testFiles,
+      reporters: ["verbose"],
+      watch: false,
+      coverage: {
+        all: true,
+        clean: true,
+        cleanOnRerun: true,
+        include: ["src"],
+        exclude: ["src/**/*.{test,spec}.{ts,tsx}", "src/main.{ts,tsx}"],
+      },
+      // biome-ignore lint/style/useNamingConvention: needed for vitest
+      env: { NODE_ENV: "test" },
+      environment: "jsdom",
+      passWithNoTests: true,
+      setupFiles: ["./vitest.setup.ts"],
     },
-    // biome-ignore lint/style/useNamingConvention: needed for vitest
-    env: { NODE_ENV: "test" },
-    environment: "jsdom",
-    passWithNoTests: true,
-    setupFiles: ["./vitest.setup.ts"],
-  },
-});
+  });
+}
+
+export default getConfig();
